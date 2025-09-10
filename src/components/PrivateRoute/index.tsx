d312bfa177b72476db23cc025066e721efbab13e
Rafael540 <alves540@hotmail.com>
2025-09-10 15:54:26 -0300

    Acess control route

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,15 +1,21 @@
 import { Navigate } from 'react-router-dom';
 import * as authService from '../../services/auth-service';
 import type { JSX } from 'react';
+import type { RoleEnum } from '../../models/auth';
 
 
 type Props = {
     children: JSX.Element;
+    roles?: RoleEnum[];
 }
 
-export function PrivateRoute({ children }: Props) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function PrivateRoute({ children, roles = [] }: Props) {
     if (!authService.isAuthenticated()) {
         return <Navigate to="/login" />;
+    }
+    if (!authService.hasAnyRoles(roles)) {
+        return <Navigate to="/catalog" />;
     }
     return children;
 }
