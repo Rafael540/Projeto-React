0177766eceda86fe50295b5351ff018f90dd99c9
Rafael540 <alves540@hotmail.com>
2025-09-16 15:10:20 -0300

    Dialog Modal alert

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product"
 import SearchBar from "../../../components/SearchBar";
 import ButtonNextPage from "../../../components/ButtonNextPage";
+import DialogInfo from "../../../components/DialogInfo";
 
 
 type QueryParams = {
@@ -92,6 +93,9 @@ export default function ProductListing() {
                     <ButtonNextPage onNextPage={handleNextPageClick} />
                 }
             </section>
+            <DialogInfo>
+                
+            </DialogInfo>
         </main>
     );
 }
