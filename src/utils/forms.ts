0bc7fc3bdc66e77d14b053001689c4386d3369ab
Rafael540 <alves540@hotmail.com>
2025-09-23 17:22:49 -0300

    Forms updated all

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,15 +1,25 @@
 /* eslint-disable no-var */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function update(inputs: any, name: string, newValue: any){
-    return {...inputs, [name]: { ...inputs[name], value: newValue }};
+export function update(inputs: any, name: string, newValue: any) {
+    return { ...inputs, [name]: { ...inputs[name], value: newValue } };
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function toValues(inputs: any){
+export function toValues(inputs: any) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const data: any = {};
     for (var name in inputs) {
         data[name] = inputs[name].value;
     }
     return data;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function updateAll(inputs: any, newValues: any) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const newInputs: any = {};
+    for (var name in inputs) {
+        newInputs[name] = { ...inputs[name], value: newValues[name] };
+    }
+    return newInputs;
 }
