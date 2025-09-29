297a91ff283e9f927bab47fe6560ff3c3bd25c9c
Rafael540 <alves540@hotmail.com>
2025-09-29 15:43:38 -0300

    forms toDirty

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -43,6 +43,10 @@ export default function ProductForm() {
     })
 
     useEffect(() => {
+
+        const result = forms.toDirty(formData, "price");
+        console.log(result);
+
         if (isEditing) {
             productService.findById(Number(params.productId))
                 .then(response => {
@@ -58,6 +62,11 @@ export default function ProductForm() {
         setFormData(dataValidated);
     }
 
+    function handleTurnDirty(name: string) {
+        const newFormData = forms.toDirty(formData, name);
+        setFormData(newFormData);
+    }
+
     return (
         <main>
             <section id="product-form-section" className="dsc-container">
@@ -69,6 +78,7 @@ export default function ProductForm() {
                                 <FormInput
                                     {...formData.name}
                                     className="dsc-form-control "
+                                    onTurnDirty={handleTurnDirty}
                                     onChange={handleInputChange}
                                 />
                                 <div className='dsc-form-error'>
@@ -79,9 +89,10 @@ export default function ProductForm() {
                                 <FormInput
                                     {...formData.price}
                                     className="dsc-form-control "
+                                    onTurnDirty={handleTurnDirty}
                                     onChange={handleInputChange}
                                 />
-                                 <div className='dsc-form-error'>
+                                <div className='dsc-form-error'>
                                     {formData.price.message}
                                 </div>
                             </div>
@@ -89,6 +100,7 @@ export default function ProductForm() {
                                 <FormInput
                                     {...formData.imgUrl}
                                     className="dsc-form-control "
+                                    onTurnDirty={handleTurnDirty}
                                     onChange={handleInputChange}
                                 />
                             </div>
